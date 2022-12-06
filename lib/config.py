ae50c06ac399b63c183f7171ad9e4db77bec3bcd
Zernez <fernando.pannullo@yahoo.it>
2022-12-06 10:46:58 +0100

    Updated Training part tested and GPU allowed

--- lib/config.py
+++ lib/config.py
@@ -114,7 +114,7 @@ G_refiner2_loss,D_decider2_loss,D_rr_acc,D_rf_acc,D_fr_acc,D_decider_rr_acc,D_de
 #        self.DEVICE = torch.device('cpu')
         self.N_WORKERS = 8
 #        self.N_GPUS = 2
-        self.N_GPUS= 2
+        self.N_GPUS= int(torch.cuda.device_count())
 
         ## Logging
         # self.N_PRINT_BATCH = 50
